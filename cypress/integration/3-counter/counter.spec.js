7b6faa0a97a408d12cc5f5a1cd3937157706285f
shin-eunji <dmswlckato@gmail.com>
2022-02-03 22:50:41 +0900

    feat: counter 초기값 테스트 완료

--- cypress/integration/3-counter/counter.spec.js
+++ cypress/integration/3-counter/counter.spec.js
@@ -1,9 +1,16 @@
-describe("example counter app", () => {
+// - [X] counter의 초기값은 0이다.
+// - [ ] + 버튼을 클릭 시, count가 1 증가한다.
+// - [ ] - 버튼을 클릭 시, count가 1 감소한다.
+// - [ ] + 버튼을 눌렀을 때, count가 10이 넘는 경우 더 이상 증가하지 못한다. (Max 값이 10이다.)
+// - [ ] - 버튼을 눌렀을 때, count는 0보다 작아지는 경우 감소하지 못한다. (Min 값이 0이다.)
+// - [ ] reset 버튼을 누르면 counter가 0으로 초기화된다.
+
+describe("카운터 앱 테스트", () => {
   beforeEach(() => {
    cy.visit("http://127.0.0.1:8080/");
   });
 
-  it("최초에 카운터 값을 0으로 보여준다.", () => {
+  it("counter의 초기값은 0이다.", () => {
     cy.get("#value").invoke("text").should("eq", "0");
   })
 });
