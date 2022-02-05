a6990ddb4e6d3bba035c17829ffac7bb0bb8ff6a
shin-eunji <dmswlckato@gmail.com>
2022-02-05 21:24:41 +0900

    feat: decrease test code

--- cypress/integration/3-counter/counter.spec.js
+++ cypress/integration/3-counter/counter.spec.js
@@ -9,18 +9,34 @@ describe("카운터 앱 테스트", () => {
   beforeEach(() => {
    cy.visit("http://127.0.0.1:8080/");
   });
-
+  // 기존값 0인지 테스트한다.
   it("counter의 초기값은 0이다.", () => {
     cy.get("#value").invoke("text").should("eq", "0");
   })
 
   it("+버튼을 클릭 시, count가 1 증가한다.", () => {
+    // 기존 값인 0을 가져온다.
     cy.get("#value").invoke("text").then((value) => {
       const preValue = Number(value);
+      // +버튼을 클릭한다.
       cy.get(".increase-btn").click();
+      // 변화된 값이 기존값 1인지 체크한다.
       cy.get("#value")
         .invoke("text")
         .should("eq", String(preValue + 1));
     });
   });
+
+  it("- 버튼을 클릭 시, count가 1 감소한다.", () => {
+    // +버튼을 클릭해서 값을 1로 만든다.
+    cy.get(".increase-btn").click();
+    // 기존 값인 1을 가져온다.
+    cy.get("#value").invoke("text").then(value => {
+      const preValue = Number(value);
+      // -버튼을 클릭한다.
+      cy.get(".decrease-btn").click();
+      // 변화된 값이 기존값 0인지 체크한다.
+      cy.get("#value").invoke("text").should("eq", String(preValue - 1));
+    });
+  })
 });
